51f26b4191155c8adc6cf2074d109d61ec8e63c3
naipeblanco <>
2018-11-14 01:05:57 +0100

    SON LAS 1:04 DEL MIÉRCOLES 14/11/2018, tendría que estar durmiendo, pero aquí estoy,empezando.

---
+++ indexVeterinaria.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=yes">
+        <title >CÁLLESE_VIEJO_LESBIANO</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/fontawesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/regular.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/solid.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container" id ="principal">
+            
+            <div>esto funciona</div>
+            
+        </div>
+       
+    </body>
+    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+     <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+</html>
